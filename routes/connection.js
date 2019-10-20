ca735563821b711805fc59293915c5aef1dfa1b9
18alantom <2.alan.tom@gmail.com>
2019-10-20 14:40:14 +0530

    Ready API server for heroku

--- routes/connection.js
+++ routes/connection.js
@@ -1,11 +1,14 @@
 // Database helpers
+require('dotenv').config()
 const { getConnection } = require("../db-helper/db-connection");
 const databaseParameters = {
-  host: "localhost",
-  user: "fetzen_master",
-  password: "fetzen_master",
-  database: "fetzen",
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABSE,
+  port: process.env.DB_PORT,
   dateStrings: true
 };
+
 const connection = getConnection(databaseParameters);
 module.exports = connection;
